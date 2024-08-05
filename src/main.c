c3ec959326e673d8a76b729304678c26501f4c2f
bbbbbr <bbbbbr@users.noreply.github.com>
2024-08-04 22:03:50 -0700

    Support for more menu items

--- src/main.c
+++ src/main.c
@@ -10,29 +10,31 @@
 
 #include "input.h"
 
-//#include "GBDK_2020_logo.h"
+
+#define ARRAY_LEN(A)  sizeof(A) / sizeof(A[0])
+
+#define CURSOR_X 1u
+#define MENU_X 4u
+#define MENU_Y 2u
+#define MENU_Y_ENTRY_SPACING 2u
+
 
 static void cart_launch_game(uint8_t selected_game);
 void init_gfx(void);
 void draw_menu(void);
 
-static volatile uint8_t AT(0x0002) CART_GAMELOAD_REG;
+uint8_t cursor_num = 0u;
+uint8_t cursor_num_last = 0u;
 
-#define ARRAY_LEN(A)  sizeof(A) / sizeof(A[0])
-
-#define CURSOR_X 1u
-
-#define MENU_X 4u
-#define MENU_Y 3u
-#define MENU_Y_ENTRY_SPACING 2u
-#define MENU_Y_ENTRY_1 (MENU_Y)
-#define MENU_Y_ENTRY_2 (MENU_Y + MENU_Y_ENTRY_SPACING)
 
 const char * menu_entries[] = {
   // 16 characters long max
   // 01234567890123456
     "Load slot 1",
     "Load slot 2",
+//    "Load slot 3",
+//    "Load slot 4",
+//    "Load slot 5",
 };
 
 #define CURSOR_MIN 0
@@ -41,16 +43,15 @@ const char * menu_entries[] = {
 #define LOAD_GAME_MSG_X 1u
 #define LOAD_GAME_MSG_Y (MENU_Y + (MENU_Y_ENTRY_SPACING * (ARRAY_LEN(menu_entries) + 1)))
 
-const uint8_t cursor_draw_ypos[] = { MENU_Y_ENTRY_1, MENU_Y_ENTRY_2 };
-uint8_t cursor_num = 0u;
-uint8_t cursor_num_last = 0u;
-
-
-
 
 
 // Writes the specified game slot to the cart register
 // which should trigger a reboot with that game enabled
+//
+// Expected values: 1 for game slot 1, 2 for game slot 2
+//
+static volatile uint8_t AT(0x0002) CART_GAMELOAD_REG;
+//
 static void cart_launch_game(uint8_t selected_game) {
 
     gotoxy(LOAD_GAME_MSG_X, LOAD_GAME_MSG_Y);
@@ -60,24 +61,26 @@ static void cart_launch_game(uint8_t selected_game) {
 }
 
 
+
 void draw_menu(void) {
 
-    gotoxy(MENU_X, MENU_Y_ENTRY_1);
-    printf("%s", menu_entries[0]);
-
-    gotoxy(MENU_X, MENU_Y_ENTRY_2);
-    printf("%s", menu_entries[1]);
+    uint8_t draw_y = MENU_Y;
+    for (uint8_t c = 0; c < ARRAY_LEN(menu_entries); c++) {
+        gotoxy(MENU_X, draw_y);
+        printf("%s", menu_entries[c]);
+        draw_y += MENU_Y_ENTRY_SPACING;
+    }
 }
 
 
 void draw_cursor(void) {
 
     // Erase the last cursor location
-    gotoxy(CURSOR_X, cursor_draw_ypos[cursor_num_last]);
+    gotoxy(CURSOR_X, MENU_Y + (MENU_Y_ENTRY_SPACING * cursor_num_last));
     printf("  ");
 
     // Draw CURSOR_X last cursor location
-    gotoxy(CURSOR_X, cursor_draw_ypos[cursor_num]);
+    gotoxy(CURSOR_X, MENU_Y + (MENU_Y_ENTRY_SPACING * cursor_num));
     printf("->");
 
     // Save last cursor position
